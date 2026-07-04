6f65779b5ba7434feee0bd70e047023f7eb8bc6a
Leo Bridger <leo.bridger@amber-tx.com>
2026-07-04 21:42:37 +0100

    initial attempt at project 4

--- projects/4/fill/Fill.asm
+++ projects/4/fill/Fill.asm
@@ -7,4 +7,85 @@
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
 
-//// Replace this comment with your code.
+
+@SCREEN
+D=A
+@screen_address_current
+M=D
+@screen_address_base
+M=D
+@24576
+D=A
+@screen_address_final
+M=D
+
+
+(LOOP)
+@24576
+D=M
+@WRITE_BLACK_SCREEN
+D;JGT
+@WRITE_WHITE_SCREEN
+0;JMP
+@LOOP
+0;JMP
+
+(WRITE_WHITE_SCREEN)
+// set current address to base address
+@screen_address_base
+D=M
+@screen_address_current
+M=D
+
+(WRITE_WHITE_SCREEN_LOOP)
+// set current address to all white
+@screen_address_current
+A=M
+M=0
+
+// increment current address
+@screen_address_current
+M=M+1
+D=A
+
+// if at end go back to loop
+@screen_address_final
+D=M
+@screen_address_current
+A=M
+D=D-A
+@WRITE_WHITE_SCREEN_LOOP
+D;JGT
+@LOOP
+0;JMP
+
+
+(WRITE_BLACK_SCREEN)
+// set current address to base address
+@screen_address_base
+D=M
+@screen_address_current
+M=D
+
+(WRITE_BLACK_SCREEN_LOOP)
+// set current address to all black
+@screen_address_current
+A=M
+M=-1
+
+// increment current address
+@screen_address_current
+M=M+1
+D=A
+
+// if at end go back to loop
+@screen_address_final
+D=M
+@screen_address_current
+A=M
+D=D-A
+@WRITE_BLACK_SCREEN_LOOP
+D;JGT
+@LOOP
+0;JMP
+
