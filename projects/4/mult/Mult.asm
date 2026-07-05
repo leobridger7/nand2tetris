21b0de435dc637664b7e216cf71250ce26b44c03
leobridger7 <57092932+leobridger7@users.noreply.github.com>
2026-07-05 12:47:57 +0100

    Completion of project 4

--- projects/4/mult/Mult.asm
+++ projects/4/mult/Mult.asm
@@ -6,4 +6,49 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
-//// Replace this comment with your code.
+
+// initialise memory
+
+@sum
+M=0
+@count
+M=0
+@R2
+M=0
+
+(LOOP)
+
+// increment count by 1
+@count
+M=M+1
+
+// when count greater than R0, goto end
+@count
+D=M
+@R0
+A=M
+D=D-A
+@PUT_RESULT
+D;JGT
+
+// increment total by R1
+@R1
+D=M
+@sum
+M=D+M
+
+@LOOP
+0;JMP
+
+(PUT_RESULT)
+
+// put sum in R2
+@sum
+D=M
+@R2
+M=D
+
+(END)
+
+@END
+0;JMP
